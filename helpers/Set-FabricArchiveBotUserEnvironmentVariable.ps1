d8247e1a92d092506a37ff646ba84d80e32bfe16
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2025-01-20 15:08:46 -0600

    Fix path resolution for Config.json in Set-FabricArchiveBotUserEnvironmentVariable.ps1

--- helpers/Set-FabricArchiveBotUserEnvironmentVariable.ps1
+++ helpers/Set-FabricArchiveBotUserEnvironmentVariable.ps1
@@ -1,7 +1,7 @@
 # Create a user environment variable called FabricArchiveBot_ConfigObject with the value from the Config.json file
 
 # Get the content of the Config.json file
-$ConfigObject = Get-Content -Path "$PSScriptRoot\..\Config.json"
+$ConfigObject = Get-Content -Path "$(Split-Path $PSScriptRoot)\Config.json"
 
 # Remove all new lines, carriage returns, and whitespace from the ConfigObject
 $ConfigObject = $ConfigObject -replace '\r\n', '' -replace '\n', '' -replace '\r', '' -replace '\s', ''
