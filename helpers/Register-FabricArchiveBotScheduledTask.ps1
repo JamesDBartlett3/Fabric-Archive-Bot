957305674414b60e01195fb09f678c5d9bcf018a
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2025-01-20 15:07:16 -0600

    Enhance scheduled task registration script for Fabric Archive Bot with improved task description and dynamic command execution

--- helpers/Register-FabricArchiveBotScheduledTask.ps1
+++ helpers/Register-FabricArchiveBotScheduledTask.ps1
@@ -1,13 +1,15 @@
 param(
   [string]$TaskName = "FabricArchiveBot",
-  [string]$TaskDescription = "Fabric Archive Bot",
-  [string]$TaskCommand = "pwsh.exe -ExecutionPolicy Bypass -File C:\FabricArchiveBot\Export-FabricItemsFromAllWorkspaces.ps1",
+  [string]$TaskDescription = "Fabric Archive Bot - Automatically create a daily archive of items from all Power BI/Fabric workspaces",
+  [string]$TaskCommand = """$((Get-Command pwsh.exe).Source)""",
+  [string]$TaskArguments = "-NoProfile -NoLogo -ExecutionPolicy Bypass -WindowStyle Hidden -File Export-FabricItemsFromAllWorkspaces.ps1",
   [string]$TaskTime = "00:00",
-  [string]$TaskUser = "LOCALSERVICE"
+  [string]$TaskUser = ([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)
 )
 
 $taskTrigger = New-ScheduledTaskTrigger -Daily -At $TaskTime
-$taskAction = New-ScheduledTaskAction -Execute $TaskCommand
+$taskAction = New-ScheduledTaskAction -Execute $TaskCommand -Argument $TaskArguments -WorkingDirectory $(Split-Path $PSScriptRoot)
 $taskPrincipal = New-ScheduledTaskPrincipal -UserId $TaskUser -LogonType ServiceAccount
 
+# Register the scheduled task
 Register-ScheduledTask -TaskName $TaskName -Description $TaskDescription -Trigger $taskTrigger -Action $taskAction -Principal $taskPrincipal
